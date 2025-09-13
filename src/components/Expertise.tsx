9157732a723d68b0d7630b92730899feca18ea7a
sanjanasujay <sanjana121123@gmail.com>
2025-09-13 14:52:27 -0700

    Add expertise

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 const labelsFirst = [
     "React",
@@ -14,25 +15,25 @@ const labelsFirst = [
 ];
 
 const labelsSecond = [
+    "Python",
+    "Java",
+    "C/C++",
+    "MySQL",
     "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "GitHub"
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Python",
+    "Pandas",
+    "NumPy",
+    "Natural Language Processing",
+    "Neural Machine Translation",
+    "Random Forests",
+    "k-Nearest Neighbors", 
+    "Logistic Regression",
+    "Support Vector Machines",
+    "Clustering methods",
 ];
 
 function Expertise() {
@@ -44,7 +45,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faReact} size="3x"/>
                     <h3>Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as HTML, CSS, JavaScript,React, and TypeScript. I have a strong proficiency in the SDLC process and frontend development.</p>
+                    <p>I have built a diverse array of web applications from scratch using modern technologies such as HTML, CSS, JavaScript, React, and TypeScript. I have a strong proficiency and experience in the SDLC process and frontend development.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -54,9 +55,10 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faCode} size="3x"/>
+                   <h3>Software Development</h3>
+                    <p>With a strong CS foundation from my UG and ongoing MS, I bring hands-on experience in data structures, algorithms, database management, and software design principles, while adhering to Agile and SDLC practices</p>
+                   
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -67,8 +69,9 @@ function Expertise() {
 
                 <div className="skill">
                     <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <h3>AI & ML</h3>
+                    <p>AI/ML practitioner with experience in NLP and transformer models, including multilingual translation research.</p>
+                    <p>Interested in Data Science and ML, with experience applying techniques such as Random Forests, k-Nearest Neighbors, Logistic Regression, Support Vector Machines, and Clustering methods to real-world datasets</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
