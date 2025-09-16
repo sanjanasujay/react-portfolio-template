fdc182be50d6a9d85a2b39fe9530ba3c7a427c7c
sanjanasujay <sanjana121123@gmail.com>
2025-09-15 22:48:24 -0700

    Remove unused code

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,7 +2,6 @@
 import mock04 from '../assets/images/mock04.png';
 import mock05 from '../assets/images/mockk.png';
 
-import mock10 from '../assets/images/mock10.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -10,19 +9,19 @@ function Project() {
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
         <div className="projects-grid">
-            <div className="project" >
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+            <div className="project"  >
+               <img src={mock05} className="zoom" alt="thumbnail" width={"60%"}   />
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Translation of Indic Languages to English and vice-versa using NLP</h2></a>
                 <p>Built a transformer-based neural machine translation model to translate multiple Indian languages to English and vice-versa. Integrated text-to-speech functionality to generate audio output of translations, enhancing accessibility for non-English speakers.</p>
             </div>
             
             <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>Café Database Management System</h2></a>
+                <img src={mock05} className="zoom" alt="thumbnail" width={"60%"}/>
+                <h2>Café Database Management System</h2>
                 <p>Developed a full-stack food ordering website using HTML, CSS, JavaScript, PHP, MySQL with dynamic menu-based ordering, enabling real-time order tracking. Through optimized MySQL queries and schema design, ensured smooth transaction handling.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="60%"/></a>
                 <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
                 <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
             </div>
