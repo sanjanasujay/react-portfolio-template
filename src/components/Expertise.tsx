db7dd3bba64c949d1fe78c62dfc88a7a47842f64
sanjanasujay <sanjana121123@gmail.com>
2025-09-15 22:50:17 -0700

    Remove fa-docker

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faReact,  faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 import { faCode } from "@fortawesome/free-solid-svg-icons";
