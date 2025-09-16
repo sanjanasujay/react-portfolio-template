34636c53cbb1b6c11fbf859cd3d629c2e8585ca2
sanjanasujay <sanjana121123@gmail.com>
2025-09-15 22:11:22 -0700

    Add about job

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,27 +16,27 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="September 2023 - June 2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Content Developer- Hewlett Packard Enterprise</h3>
+            <h3 className="vertical-timeline-element-title">Content Developer - Hewlett Packard Enterprise</h3>
             <h4 className="vertical-timeline-element-subtitle">Bengaluru, India</h4>
-            <p>
-               Web Development, Project Management, WAIT
-            </p>
+            <p>Developed web-based training courses on HPE's storage products using React JS and TypeScript.</p>
+            <p>Delivered technical training courses at HPE covering Storage Area Network (SAN) technologies, enterprise storage servers, and big data solutions such as HPE Ezmeral Runtime Enterprise, as well as cloud-focused offerings including HPE Private Cloud AI and HPE GreenLake solutions.</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="March 2023 - August 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Content Developer Intern - Hewlett Packard Enterprise</h3>
             <h4 className="vertical-timeline-element-subtitle">Bengaluru, India</h4>
             <p>
-              Web Development, Project Management, WAIT
+              Developed web-based training courses on HPE's storage products using HTML, CSS, JavaScript.
             </p>
+            <p>Delivered technical training courses at HPE on enterprise storage servers and on cloud offerings of HPE GreenLake.</p>
           </VerticalTimelineElement>
           
         </VerticalTimeline>
