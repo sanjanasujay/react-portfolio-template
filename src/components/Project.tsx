2ee283a8a59526162775789ab368c1283c4cd5b2
sanjanasujay <sanjana121123@gmail.com>
2025-09-15 23:06:03 -0700

    Fix size

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
 
 import mock04 from '../assets/images/mock04.png';
 import mock05 from '../assets/images/mockk.png';
+import mock06 from '../assets/images/mock5.png';
 
 import '../assets/styles/Project.scss';
 
@@ -10,13 +11,13 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project"  >
-               <img src={mock05} className="zoom" alt="thumbnail" width={"60%"}   />
+               <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}   />
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Translation of Indic Languages to English and vice-versa using NLP</h2></a>
                 <p>Built a transformer-based neural machine translation model to translate multiple Indian languages to English and vice-versa. Integrated text-to-speech functionality to generate audio output of translations, enhancing accessibility for non-English speakers.</p>
             </div>
             
             <div className="project">
-                <img src={mock05} className="zoom" alt="thumbnail" width={"60%"}/>
+                <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}/>
                 <h2>Café Database Management System</h2>
                 <p>Developed a full-stack food ordering website using HTML, CSS, JavaScript, PHP, MySQL with dynamic menu-based ordering, enabling real-time order tracking. Through optimized MySQL queries and schema design, ensured smooth transaction handling.</p>
             </div>
@@ -24,6 +25,11 @@ function Project() {
                 <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="60%"/></a>
                 <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
                 <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
+            </div>
+             <div className="project">
+                <img src={mock06} className="zoom" alt="thumbnail" width="80%"/>
+              <h2>Building a machine learning project</h2>
+                <p></p>
             </div>
            
            
