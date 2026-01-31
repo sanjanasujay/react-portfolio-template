7038f4aec84fa3737d13eb878b59bff778a465b3
sanjanasujay <sanjana121123@gmail.com>
2026-01-31 15:05:18 -0800

    Add image zooming

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import CloseIcon from '@mui/icons-material/Close';
 import '../assets/styles/Main.scss';
 import photo from "../assets/images/profilePicture.png";
 
 function Main() {
 
+  const [isZoomed, setIsZoomed] = useState(false);
+
   return (
     <div className="container">
-      <div className="about-section">
+      <div className={`about-section ${isZoomed ? 'zoomed-active' : ''}`}>
         <div className="image-wrapper">
-          <img src={photo} alt="Avatar" />
+          <img 
+            src={photo} 
+            alt="Avatar" 
+            onClick={() => setIsZoomed(true)}
+            className="profile-image"
+          />
+          {isZoomed && (
+            <div className="zoom-modal">
+              <div className="zoom-content">
+                <button className="close-button" onClick={() => setIsZoomed(false)}>
+                  <CloseIcon />
+                </button>
+                <img src={photo} alt="Avatar Zoomed" className="zoomed-image" />
+              </div>
+            </div>
+          )}
         </div>
         <div className="content">
           <div className="social_icons">
