1ad6f79f20c558c648dad105789e6aaaa33f31a5
sanjanasujay <sanjana121123@gmail.com>
2026-01-31 14:39:50 -0800

    Add career history

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -20,9 +20,22 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
+            <h3 className="vertical-timeline-element-title">Student Assistant - San Diego State University</h3>
+            <h4 className="vertical-timeline-element-subtitle">San Diego, CA</h4>
+            <p>Managed digital content for the College of Sciences website via Modern Campus. Concurrently developed Python scripts to automate research data analysis, cutting manual processing time by 30% and eliminating human error to improve data integrity.</p>
+            
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="September 2023 - June 2025"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
             <h3 className="vertical-timeline-element-title">Content Developer - Hewlett Packard Enterprise</h3>
             <h4 className="vertical-timeline-element-subtitle">Bengaluru, India</h4>
-            <p>Developed web-based training courses on HPE's storage products using React JS and TypeScript.</p>
+            <p>Developed web-based training courses on HPE's storage products using React JS, TypeScript, JavaScript.</p>
             <p>Delivered technical training courses at HPE covering Storage Area Network (SAN) technologies, enterprise storage servers, and big data solutions such as HPE Ezmeral Runtime Enterprise, as well as cloud-focused offerings including HPE Private Cloud AI and HPE GreenLake solutions.</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
